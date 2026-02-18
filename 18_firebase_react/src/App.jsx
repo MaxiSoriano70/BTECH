69e48ec6586e703f02943321762f8f0eae9d8e39
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2026-02-18 17:16:45 -0300

    Clase 18 FireBase

--- 18_firebase_react/src/App.jsx
+++ 18_firebase_react/src/App.jsx
@@ -6,29 +6,33 @@ function App() {
   const [text, setText] = useState("");
   const [products, setProducts] = useState([]);
 
-  // 🔹 Traer productos al cargar el componente
+  //Obtiene los productos de la base de datos
   useEffect(() => {
     const getProducts = async () => {
+      //Trae los productos de la base de datos y los guarda en el estado local
       const querySnapshot = await getDocs(collection(db, "products"));
       const productsData = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         name: doc.data().name,
       }));
+      //Guarda los productos en el estado local
       setProducts(productsData);
     };
 
     getProducts();
   }, []);
 
-  // 🔹 Agregar producto
+  //Agregar producto
   async function handleAdd() {
+    //Si el input esta vacio no se agrega nada
     if (text.trim() === "") return;
-
+    //Agrega un nuevo producto a la base de datos
     const docRef = await addDoc(collection(db, "products"), {
       name: text,
     });
-
+    //Agrega el nuevo producto al estado local
     setProducts([...products, { id: docRef.id, name: text }]);
+    //Limpia el input
     setText("");
   }
 
